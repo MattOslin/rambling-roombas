77d880b6095d615142fa58d64b928461ef8d5858
Garrett Wenger <garrettwenger@gmail.com>
2016-11-22 13:04:01 -0500

    Fixed includes in localize.h so that the code compiles

--- atmega/inc/localize.h
+++ atmega/inc/localize.h
@@ -1,10 +1,10 @@
 #ifndef LOCALIZE_H_
 #define LOCALIZE_H_
 
-#include "diff_drive.h"
+#include "init.h"
 #include "m_wii.h"
-#include <stdbool.h>
-#include <stdint.h>
+// #include <stdbool.h>
+// #include <stdint.h>
 
 void localize_init(void);
 bool localize_wii(pos* posStruct);
