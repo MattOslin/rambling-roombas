50c0c626f1e25eb4f4b9d5108a9086952674431c
Garrett Wenger <garrettwenger@gmail.com>
2016-12-04 01:11:21 -0500

    Added first dead reckoning code and working on more aggressively throwing out bad constellation points

--- atmega/inc/localize.h
+++ atmega/inc/localize.h
@@ -5,6 +5,7 @@
 #include "m_wii.h"
 
 void localize_init(void);
+void localize_enc(pos* posStruct, double encCountsL, double encCountsR);
 bool localize_wii(pos* posStruct);
 bool localize_cal(void);
 
